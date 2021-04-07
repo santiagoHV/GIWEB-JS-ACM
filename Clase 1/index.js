bc611b1dd5a3eff1be986bb78d6cd14e35197889
santiagoHV <santiherreravelas@gmail.com>
2021-04-07 10:57:35 -0500

    clase 1

---
+++ Clase 1/index.js
@@ -0,0 +1,26 @@
+//Variables son declaradas con var sin importar su tipo
+var saludo = 'hola'
+
+console.log(saludo)
+
+// ; Es opcional
+
+//comentario
+
+/*
+comentario de varias lineas
+*/
+
+//Tipos de datos
+4;
+4.5;
+
+'cadena de strings';
+
+true;
+false;
+
+[]; {};
+
+null;
+undefined;
